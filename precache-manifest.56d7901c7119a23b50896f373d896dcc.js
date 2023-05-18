de066abeb63252297d15797a12ca2429e6e4cc2b
Luiggi Tenorio Ku <luiggi.tenorio@pucp.pe>
2023-05-18 20:58:01 +0200

    Updates

---
+++ precache-manifest.56d7901c7119a23b50896f373d896dcc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "674f8ebb8fddc6e272319ff9a5069fc4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7a6b410bbcf0ba1437b",
+    "url": "/static/css/2.a67d0b00.chunk.css"
+  },
+  {
+    "revision": "7a6db7319e4c8e55ca9c",
+    "url": "/static/css/main.ddb06a79.chunk.css"
+  },
+  {
+    "revision": "f7a6b410bbcf0ba1437b",
+    "url": "/static/js/2.23a6c8e0.chunk.js"
+  },
+  {
+    "revision": "6cb2abd69bcf5c1bc8ea13bc6a30ce0e",
+    "url": "/static/js/2.23a6c8e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a6db7319e4c8e55ca9c",
+    "url": "/static/js/main.9a624043.chunk.js"
+  },
+  {
+    "revision": "f0f9f9ae314862dc1e84",
+    "url": "/static/js/runtime-main.d90b4988.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
