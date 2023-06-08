575c590bdd2928d866123e840ff9a11772a559ae
Luiggi Tenorio Ku <luiggi.tenorio@pucp.pe>
2023-06-08 13:01:20 +0200

    add analyze function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Profiler } from 'react';
 import './App.css';
 import './variables.css'
 import Landing from './views/Landing';
@@ -6,13 +6,22 @@ import { HashRouter, Routes, Route } from 'react-router-dom';
 import AutosubmitGraph from './views/AutosubmitGraph';
 
 function App() {
+    function onRender(id, phase, actualDuration, baseDuration, startTime, commitTime) {
+        console.log(`${id}'s ${phase} phase:`);
+        console.log(`Actual time: ${actualDuration}`);
+        console.log(`Base time: ${baseDuration}`);
+        console.log(`Start time: ${startTime}`);
+        console.log(`Commit time: ${commitTime}`);
+    }
     return (
-        <HashRouter>
-            <Routes>
-                <Route path="/" element={<Landing />} />
-                <Route path="autosubmitgraph" element={<AutosubmitGraph />} />
-            </Routes>
-        </HashRouter>
+        <Profiler id="App" onRender={onRender}>
+            <HashRouter>
+                <Routes>
+                    <Route path="/" element={<Landing />} />
+                    <Route path="autosubmitgraph" element={<AutosubmitGraph />} />
+                </Routes>
+            </HashRouter>
+        </Profiler>
     );
 }
 
