0ad20799fbf313e17588e916c1a617e81b86c734
Luiggi Tenorio Ku <luiggi.tenorio@pucp.pe>
2023-06-08 10:50:27 +0200

    Updates

---
+++ precache-manifest.faa08f45c1c7d9a9b85a89daeb77a341.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40109d00d5e49e65d3fb7c425ea675c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66566a3b6b728e274d01",
+    "url": "/static/css/2.a67d0b00.chunk.css"
+  },
+  {
+    "revision": "c836d5cfdea7d24b1d01",
+    "url": "/static/css/main.4aa7868b.chunk.css"
+  },
+  {
+    "revision": "66566a3b6b728e274d01",
+    "url": "/static/js/2.c6d31d23.chunk.js"
+  },
+  {
+    "revision": "6be3ab46adfcfb6595c1fa4f18a12448",
+    "url": "/static/js/2.c6d31d23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c836d5cfdea7d24b1d01",
+    "url": "/static/js/main.06f2ecda.chunk.js"
+  },
+  {
+    "revision": "f0f9f9ae314862dc1e84",
+    "url": "/static/js/runtime-main.d90b4988.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7e496ccb075606083ecca562ded0767a.js"
+  "/precache-manifest.faa08f45c1c7d9a9b85a89daeb77a341.js"
 );
 
 self.addEventListener('message', (event) => {
